441c0ecf66d25b7f5749b7ab5bf9234a56e20f35
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-08-06 17:14:24 +0330

    infinite scroll created

--- src/js/main.js
+++ src/js/main.js
@@ -5,58 +5,103 @@ window.addEventListener("DOMContentLoaded", App);
 function App() {
   Mount(Header());
   Mount(Main());
-  Mount(MoviesBoxesSlider(0 , 20), document.getElementById("main-container"));
-  Mount(MoviesBoxesSlider(21, 40), document.getElementById("main-container"));
-  Mount(MoviesBoxesSlider(41, 60), document.getElementById("main-container"));
+  Mount(MoviesBoxesSlider(), document.getElementById("main-container"));
+  Mount(MoviesBoxesSlider(), document.getElementById("main-container"));
+  // Mount(MoviesBoxesSlider(41, 60), document.getElementById("main-container"));
 }
 
-// mount 
-let Mount = (element, place = root) => {
+// mount
+const Mount = (element, place = root) => {
   place.appendChild(element);
 };
-// unMount 
-let UnMount = (element) => {
-  element.remove();
+// unMount
+const UnMount = (element) => {
+  element.remove()
 };
-// listUnMount 
-let listUnMount = (element) => {
-  element.forEach(item => item.remove())
-}
+// listUnMount
+const listUnMount = (element) => {
+  element.forEach((item) => item.remove());
+};
 // ##get all movies##
-let GetAllMovies = async () => {
+const GetAllMovies = async () => {
   let movies;
   let URL = "https://api.tvmaze.com/shows";
 
   try {
     let response = await fetch(URL);
     let json = await response.json();
-    movies = json.filter((item, index) => index < 201);
+    movies = json.filter((item, index) => index < 321);
   } catch (err) {
     console.err(err);
   }
 
   return movies;
 };
+// loading component
+const loadingElement = () => {
+  // createElement
+  let loadingSection = document.createElement('section');
+  let loadingSectionContainer = document.createElement('div');
+  let loading = document.createElement('img');
+
+  // attributes
+  loadingSection.classList.add('loading-section');
+  loadingSectionContainer.classList.add('loading-section-container');
+  loading.classList.add('loading-el')
+  loading.alt = 'loading gif';
+  loading.src = './assets/gif/loading/loading.gif';
+
+  // appenchild
+  loadingSection.appendChild(loadingSectionContainer);
+  loadingSectionContainer.appendChild(loading);
+
+  // return
+  return loadingSection;
+}
+// infinite scroll event
+const infiniteScroll = () => {
+  window.addEventListener("scroll", () => {
+    let docHight = document.documentElement.scrollHeight;
+    let docTop = document.documentElement.scrollTop + 3;
+    let windowHeight = window.innerHeight;
+
+    if (windowHeight + docTop >= docHight) {
+      if(document.querySelector('.loading-section') === null){
+        // mount loading element in main
+        Mount(loadingElement() , document.getElementById("main-container"))
+
+        // remove loading from doc and mount new slider
+        setTimeout(() => {
+          UnMount(document.querySelector('.loading-section'));
+          Mount(MoviesBoxesSlider(41, 60), document.getElementById("main-container"));
+        }, 2000);
+      }
+
+    }
+  });
+};
+infiniteScroll();
 // side navbar Event
-let showSideNavbar = (element, target, className) => {
+const showSideNavbar = (element, target, className) => {
   element.addEventListener("click", () => {
     target.classList.add(className);
     root.parentElement.classList.add("overflow-hidden");
   });
 };
 // remove side navbar
-let removeSideNavbar = (element, target, className) => {
+const removeSideNavbar = (element, target, className) => {
   element.addEventListener("click", () => {
     target.classList.remove(className);
     root.parentElement.classList.remove("overflow-hidden");
   });
 };
 // darkmode handler
-let activeDarkmode = (
+const activeDarkmode = (
   element,
   className,
   btnTwo,
   hamburgerElement,
+  header,
   target = root.parentElement
 ) => {
   element.addEventListener("click", () => {
@@ -65,14 +110,18 @@ let activeDarkmode = (
     element.classList.add("none");
     btnTwo.classList.remove("none");
     hamburgerElement.src = "./assets/icons/menu/menu-white/menu.svg";
+    header.classList.add("header-dark");
+    // --BUG-- when darkmode clicked loading loading.src must change , now it's dosen't change becuse loading dosen't mount in app and this return null
+    document.querySelector('.loading-el').src = './assets/gif/loading/dark-loading.gif';
   });
 };
 //darkmode remover
-let inActiveDarkmode = (
+const inActiveDarkmode = (
   element,
   className,
   btnTwo,
   hamburgerElement,
+  header,
   target = root.parentElement
 ) => {
   element.addEventListener("click", () => {
@@ -81,11 +130,13 @@ let inActiveDarkmode = (
     element.classList.add("none");
     btnTwo.classList.remove("none");
     hamburgerElement.src = "./assets/icons/menu/menu-black/menu.svg";
+    header.classList.remove("header-dark");
+    document.querySelector('.loading-el').src = './assets/gif/loading/loading.gif';
   });
 };
 // components
 // ###-header-###
-let Header = () => {
+const Header = () => {
   //create header elements
   let header = document.createElement("header");
   // header container
@@ -125,8 +176,12 @@ let Header = () => {
   let navbarMoviesSelect = document.createElement("select");
   // let navbarMoviesOption = document.createElement("option");
   // ####################################################
+  // input value
+  const getInputValue = () => {};
+  // ####################################################
+  // ####################################################
   // **movies options value from API**
-  let moviesOptionValue = async () => {
+  const moviesOptionValue = async () => {
     let movieData = await GetAllMovies();
     movieData.forEach((item) => {
       let navbarMoviesOption = document.createElement("option");
@@ -142,7 +197,7 @@ let Header = () => {
   let navbarEpisodeSelect = document.createElement("select");
   let navbarEpisodeOptions = document.createElement("option");
   // ####################################################
-  let episodeOptionValue = async () => {
+  const episodeOptionValue = async () => {
     alert("hi");
   };
   // ####################################################
@@ -263,19 +318,21 @@ let Header = () => {
     darkModeBtnMoon,
     "body-darkmode",
     darkmodeBtnSun,
-    hamburgerImageBtn
+    hamburgerImageBtn,
+    header
   );
   inActiveDarkmode(
     darkmodeBtnSun,
     "body-darkmode",
     darkModeBtnMoon,
-    hamburgerImageBtn
+    hamburgerImageBtn,
+    header
   );
   // return header elemenet for mounting
   return header;
 };
 // ###-main-###
-let Main = () => {
+const Main = () => {
   //create main elements
   let main = document.createElement("main");
   let mainContainer = document.createElement("div");
@@ -290,13 +347,18 @@ let Main = () => {
   // return
   return main;
 };
+// ##-Box-##
+// const Box = () => {
+
+// }
 // ###-MoviesBoxesSlider-###
-let MoviesBoxesSlider = (step, count) => {
+let step = 0;
+let count = 10;
+const MoviesBoxesSlider = () => {
   let moviesBoxSection = document.createElement("section");
   // get data from API
   let setImgAndTitleMoviesSlider = async () => {
     let movieData = await GetAllMovies();
-    
     for (let i = step; i <= count; i++) {
       // img box
       let moviesBox = document.createElement("div");
@@ -321,29 +383,24 @@ let MoviesBoxesSlider = (step, count) => {
       moviesBoxImgContainer.appendChild(moviesBoxImg);
       moviesBox.appendChild(moviesBoxTitleContainer);
       moviesBoxTitleContainer.appendChild(moviesBoxTitleH3);
-
-      moviesBox.addEventListener("click", (Event) => {
-        console.log('id box clicked : ' , Event.target.id)
-        Mount(MoviesInformation(Event.target.id) , document.getElementById("main-container"));
-      });
     }
-    
+
+    step = count + 1;
+    count = count + 10;
   };
-  setImgAndTitleMoviesSlider(step, count);
+  setImgAndTitleMoviesSlider();
   // return
   return moviesBoxSection;
 };
 // ###-MovieInformations-###
-let MoviesInformation = (movieId) => {
+const MoviesInformation = (movieId) => {
   // unmount all sliders
   listUnMount(document.querySelectorAll(".movie-boxes"));
-  // get movieData 
+  // get movieData
   let moviesInformationSection = document.createElement("section");
+
   let configMoviesInformationPage = async () => {
     let movieData = await GetAllMovies();
-    let movieTarget = movieId;
-    console.log(movieData[movieTarget]);
-
 
     // creating moviesinformation elements
     let moviesInformationContainer = document.createElement("div");
@@ -356,14 +413,14 @@ let MoviesInformation = (movieId) => {
     // Scenes
     let moviesInformationScenesContainer = document.createElement("div");
     let moviesInformationScenesImg = document.createElement("img");
-  
+
     // classlist , src , type ...
     moviesInformationSection.classList.add("movies-information-section");
     moviesInformationContainer.classList.add("container", "movies-information");
     moviesInformationImgContainer.classList.add(
       "movie-information-image-container"
     );
-    moviesInformationImg.src = movieData[movieTarget].image.medium;
+    moviesInformationImg.src = movieData[movieId].image.medium;
     moviesInformationImg.alt = "movies-poster";
     moviesInformationDescriptionContainer.classList.add(
       "movies-information-description"
@@ -374,18 +431,20 @@ let MoviesInformation = (movieId) => {
     moviesInformationScenesImg.classList.add("movies-information-scenes-img");
     moviesInformationScenesImg.src = "./assets/img/3.jpg";
     moviesInformationScenesImg.alt = "movies-scenes";
-  
+
     // append
     moviesInformationSection.appendChild(moviesInformationContainer);
     moviesInformationContainer.appendChild(moviesInformationImgContainer);
     moviesInformationImgContainer.appendChild(moviesInformationImg);
-    moviesInformationContainer.appendChild(moviesInformationDescriptionContainer);
+    moviesInformationContainer.appendChild(
+      moviesInformationDescriptionContainer
+    );
     moviesInformationDescriptionContainer.appendChild(moviesInformationP);
     moviesInformationContainer.appendChild(moviesInformationScenesContainer);
     moviesInformationScenesContainer.appendChild(moviesInformationScenesImg);
-  }
+  };
 
-  configMoviesInformationPage()
+  configMoviesInformationPage();
 
   // return
   return moviesInformationSection;
